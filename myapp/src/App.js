d258ab750b1535ee4c99d0f6d38356a9c8b15002
Jason Worthy <109769584+4everWorthy@users.noreply.github.com>
2024-07-09 09:04:13 -0400

    1st commit

---
+++ myapp/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import Board from './componets/board';
+import Square from './componets/square';
+
+function App() {
+  return (
+   <>
+    <Board>
+    <Square></Square>
+    </Board>
+    
+   </>
+  );
+}
+
+
+export default App;
